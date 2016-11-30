b61297882de312cfad8468703ae062311bc0bfa3
Samantha Spensley <spenslsm@gmail.com>
2016-11-30 14:25:27 -0700

    new post not working

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,15 +1,49 @@
 import axios from 'axios';
 
 export const FETCH_POSTS = 'FETCH_POSTS';
+export const CREATE_POST = 'CREATE_POST';
+export const FETCH_POST = 'FETCH_POST';
+export const DELETE_POST = 'DELETE_POST';
 
 const ROOT_URL = 'http://reduxblog.herokuapp.com/api';
 const API_KEY = '?key=funkyduck22'
 
+// get all posts
 export function fetchPosts() {
   const request = axios.get(`${ROOT_URL}/posts${API_KEY}`);
-  
+
   return {
     type: FETCH_POSTS,
     payload: request
   }
+}
+
+// create new post
+export function createPost(props) {
+  const request = axios.post(`${ROOT_URL}/posts${API_KEY}`, props);
+
+  return {
+    type: CREATE_POST,
+    payload: request
+  };
+}
+
+// get post by ID
+export function fetchPost(id) {
+  const request = axios.get(`${ROOT_URL}/posts/${id}${API_KEY}`);
+
+  return {
+    type: FETCH_POST,
+    payload: request
+  };
+}
+
+// delete post by ID
+export function deletePost(id) {
+  const request = axios.delete(`${ROOT_URL}/posts/${id}${API_KEY}`);
+
+  return {
+    type: DELETE_POST,
+    payload: request
+  };
 }
--- src/components/posts_index.js
+++ src/components/posts_index.js
@@ -1,7 +1,7 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
-import { bindActionCreators } from 'redux';
 import { fetchPosts } from '../actions/index'
+import { Link } from 'react-router';
 
 class PostsIndex extends Component {
   // lifecycle method called automatically whenever component is about to be rendered for the FIRST TIME
@@ -10,11 +10,38 @@ class PostsIndex extends Component {
     this.props.fetchPosts();
   }
 
+  renderPosts() {
+    return this.props.posts.map((post) => {
+      return (
+        <li className="list-group-item" key={ post.id }>
+          <Link to={"posts/" + post.id} >
+          <span className="pull-xs-right">{ post.categories }</span>
+          <strong>{ post.title }</strong>
+          </Link>
+        </li>
+      );
+    });
+  }
+
   render() {
     return (
-      <div>List of blog posts</div>
+      <div>
+        <div className="text-xs-right">
+          <Link to="/posts/new" className="btn btn-primary">
+            Add a Post
+          </Link>
+        </div>
+        <h3>Posts</h3>
+        <ul className="list-group">
+          <li>{ this.renderPosts }</li>
+        </ul>
+      </div>
     );
   }
 }
 
-export default connect(null, { fetchPosts: fetchPosts })(PostsIndex);
+function mapStateToProps(state) {
+  return { posts: state.posts.all}
+}
+
+export default connect(mapStateToProps, { fetchPosts: fetchPosts })(PostsIndex);
